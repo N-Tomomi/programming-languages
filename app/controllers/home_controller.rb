1ca3bf162b510dfa84f1c4b12a33f6ec9a397978
N-Tomomi <d_o_yukijiro@hotmail.co.jp>
2020-03-30 12:22:17 +0900

    pagenation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,8 @@ class HomeController < ApplicationController
   end
 
   def ruby
-    # @posts = [
-    #   "今日からProgateでRailsの勉強するよー！",
-    #   "投稿一覧ページ作成中！"
-    # ]    
-    # @rubyposts = Rubypost.all
-    @rubyposts = Rubypost.all.order(created_at: :desc)
+    # @rubyposts = Rubypost.all.order(created_at: :desc)
+    @rubyposts  = Rubypost.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
@@ -19,8 +15,6 @@ class HomeController < ApplicationController
     @rubypost.save
     redirect_to ruby_path
   end
-
-  
 
 end
 
